a0fc5317ef12df84b2c940ed1600995f2150691b
VictorGuui <victorguilhermebranco@outlook.com>
2024-05-12 18:25:46 -0300

    feat: add all the Singleton Code

---
+++ Eng Software III/Singleton/src/FakeImage.java
@@ -0,0 +1,10 @@
+public class FakeImage {
+   
+  private String color;
+    public FakeImage(String color){
+        this.color = color;
+    }
+    public String getColor(){
+        return color;
+    }
+}
